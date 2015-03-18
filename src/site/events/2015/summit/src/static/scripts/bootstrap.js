a50ca24b6dc53d51ee3f991b5839848476aab668
Seth Ladd <sethladd@google.com>
2015-03-17 22:56:21 -0700

    latest files from CDS site

--- src/site/events/2015/summit/src/static/scripts/bootstrap.js
+++ src/site/events/2015/summit/src/static/scripts/bootstrap.js
@@ -1,3 +1,31 @@
+/**
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+CDS.EventPublisher.add('load', function() {
+
+  // Remove the color-blocks
+  document.body.classList.remove('deeplink-schedule');
+  document.body.classList.remove('deeplink-sessions');
+  document.body.classList.remove('deeplink-attendee-information');
+  document.body.classList.remove('deeplink-get-involved');
+  document.body.classList.remove('deeplink-about-chrome-dev-summit');
+
+  document.body.classList.add('loaded');
+
+});
+
 CDS.History.init();
 
 if ('serviceWorker' in navigator) {
